729329db3390f299c5de3f2df01929b5587372df
Craige McWhirter <craige@mcwhirter.io>
2016-10-27 21:31:19 +1000

    Completed Tutorial and watch face works.

--- src/c/simplecforpebble.c
+++ src/c/simplecforpebble.c
@@ -1,8 +1,25 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-
 static TextLayer *s_time_layer;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+    "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
 
 static void main_window_load(Window *window) {
   // Get information about the Window
@@ -41,6 +58,12 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
